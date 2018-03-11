dcbd795897da994d42c20704f89246f5760382a8
lAngelP <info@langelp.net>
2018-03-11 07:17:35 +0100

    Change twit to tweet

--- Streaming/main.py
+++ Streaming/main.py
@@ -1,3 +1,3 @@
 import streaming
 import time
-streaming.start_streaming("Jara", "uCode", ["adidas"],20181102,"Eina, Zaragoza", time.time()+1, 600)
+streaming.start_streaming("#ucodebyadidas2018", "uCode", ["adidas"],20181102,"Eina, Zaragoza", time.time()+1, 600)
